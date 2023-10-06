7abe800f913787c14957304c459d796007256062
David Atlan <david@davipro.fr>
2023-10-06 16:45:45 +0200

    The 'flutterIcon' widget was created

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'home_page.dart';
 import 'signup_page.dart';
 
@@ -87,13 +88,41 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             flutterIcon,
-            titleSection,
-            textSection,
-            InputSection(),
-            forgetButton,
+            //titleSection,
+            //textSection,
+            //InputSection(),
+            //forgetButton,
           ],
         ),
       ),
     );
   }
 }
+
+Widget flutterIcon = Container(
+  margin: const EdgeInsets.only(top: 20),
+  padding: const EdgeInsets.all(20),
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(60),
+    color: const Color.fromRGBO(255, 255, 255, 0.1),
+  ),
+  height: 200,
+  width: 200,
+  child: Container(
+    padding: EdgeInsets.all(15),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(40),
+      color: Colors.white,
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          spreadRadius: 5,
+          blurRadius: 15,
+          offset: Offset(0, 3),
+        ),
+      ],
+    ),
+    child: Logo(Logos.firebase),
+
+  ),
+);
